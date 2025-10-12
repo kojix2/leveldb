a92fc8950e2ee006e0ad4612463823bd7f9a03f2
kojix2 <2xijok@gmail.com>
2025-10-12 18:40:30 +0900

    crystal tool format

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,19 +3,19 @@ require "../src/leveldb"
 
 # Test helpers
 def make_tmpdir(prefix = "ldb-") : String
-	dir = File.join(Dir.tempdir, "#{prefix}#{Random::Secure.hex(4)}")
-	Dir.mkdir(dir)
-	dir
+  dir = File.join(Dir.tempdir, "#{prefix}#{Random::Secure.hex(4)}")
+  Dir.mkdir(dir)
+  dir
 end
 
 def rm_rf(dir : String)
-	paths = Dir.glob("#{dir}/**/*")
-	paths.reverse_each do |path|
-		if File.directory?(path)
-			Dir.delete(path) rescue nil
-		else
-			File.delete(path) rescue nil
-		end
-	end
-	Dir.delete(dir) rescue nil
+  paths = Dir.glob("#{dir}/**/*")
+  paths.reverse_each do |path|
+    if File.directory?(path)
+      Dir.delete(path) rescue nil
+    else
+      File.delete(path) rescue nil
+    end
+  end
+  Dir.delete(dir) rescue nil
 end
